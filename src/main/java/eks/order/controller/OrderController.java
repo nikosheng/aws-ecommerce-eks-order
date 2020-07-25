7403a4f486a7af7ccc54ddab60adac30a4243336
Niko <niko.js.feng@gmail.com>
2020-07-25 16:35:20 +0800

    update orderVO

--- src/main/java/eks/order/controller/OrderController.java
+++ src/main/java/eks/order/controller/OrderController.java
@@ -31,6 +31,8 @@ public class OrderController {
         OrderVO orderVO = null;
         try {
             orderVO = OrderVO.OrderVOBuilder.newBuilder()
+                     .tag("eks-order")
+                     .version("version-1")
                      .ip(InetAddress.getLocalHost().getHostAddress())
                      .orderId(order.getOrderId())
                      .customerId(order.getCustomerId())
